d241bc2caa784951ae6b2cb7c008f08ba85aa4d2
Simon Kunz <r21291@student.osu.cz>
2023-03-09 17:53:26 +0100

    init backend

---
+++ BackendToDoApp/src/main/java/com/osu/backendtodoapp/BackendToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.osu.backendtodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendToDoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendToDoAppApplication.class, args);
+    }
+
+}
---
+++ BackendToDoApp/src/test/java/com/osu/backendtodoapp/BackendToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osu.backendtodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendToDoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
